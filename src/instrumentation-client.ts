a97ebdaccbace3c20b5e45fb53b5bf0fee894ca5
rudybrrr <agnerudhresh01@gmail.com>
2026-04-03 16:57:32 +0800

    Added PostHog for user analytics

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -3,6 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import posthog from "posthog-js";
 
 Sentry.init({
   dsn: "https://0752816f456da9b8ffe1c41401c1d53f@o4511155004375040.ingest.us.sentry.io/4511155004571648",
@@ -11,5 +12,10 @@ Sentry.init({
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
 });
+
+posthog.init('phc_urUNzVHREitd7GiBZLS6VnqGz4FYSqHbvC2AVRGua8UD', {
+    api_host: 'https://us.i.posthog.com',
+    defaults: '2026-01-30'
+})
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
